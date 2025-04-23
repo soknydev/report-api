f8ae90f6521158e4c9c32ab4acadfd4678c79b62
soknydev <psnpsn667788@gmail.com>
2025-04-23 15:54:07 +0700

    allow origin

--- src/main/java/dev/reportapi/controller/AuthController.java
+++ src/main/java/dev/reportapi/controller/AuthController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
--- src/main/java/dev/reportapi/controller/TransactionReportController.java
+++ src/main/java/dev/reportapi/controller/TransactionReportController.java
@@ -4,13 +4,11 @@ import dev.reportapi.model.TransactionReport;
 import dev.reportapi.service.TransactionReportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/reports")
